1512b54bd954c76f89429e63e8cbb7acd7ae3d05
kismir <kismir@users.noreply.github.com>
2016-03-02 15:14:04 +0300

    Create constants.py

---
+++ Gmodule/constants.py
@@ -0,0 +1,27 @@
+#constants and variables
+class constants:
+
+    ##### Visualization
+    wsize=(800,600)
+    
+    light=(-1,-1,100)
+    #direct=(0,0,1)
+    c=100,100
+    scale_factor=10
+    cmass_xyz=(150,80,100)
+
+    rot_factor=100 # this is z component of ruling vector
+    base_vec=(0,0,1)
+    start_rot=(0,0)
+    rot_axis=(1,0,0)
+    angl=0
+
+    position_xy=(wsize[0]/2,wsize[1]/2)
+    cur_position_xy=position_xy
+    cursor_start_xy=(0,0)
+
+    vect=[]
+    subvect=[]
+
+    ##### Widgets controlling
+    Gwidgets = 1
