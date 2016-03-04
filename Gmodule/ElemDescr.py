b4cb9267d23105348f6f01a365a87f5dc71ce657
kismir <kismir@users.noreply.github.com>
2016-03-04 12:08:42 +0300

    Create ElemDescr.py

---
+++ Gmodule/ElemDescr.py
@@ -0,0 +1,18 @@
+# Here elements, edges , vertex defines
+
+class vertex:
+    number=None
+    x=None
+    y=None
+    z=None
+
+class edge:
+    number=None
+    vertexTail=None # Here comes class vertex
+    vertexHead=None # Here comes class vertex
+
+class element:
+    number=None
+    edgeList=[]
+
+    
