504c146c451eaddfe8e27f80e82a4603507869b4
kismir <kismir@users.noreply.github.com>
2016-07-06 16:23:48 +0300

    Create ex0.py

---
+++ examples/ex0.py
@@ -0,0 +1,54 @@
+from threading import Thread
+import time
+import sys
+import os
+# add module folders to path
+sys.path.append(os.getcwd()+'/Gmodule')
+sys.path.append(os.getcwd()+'/MSHmodule')
+import gExtract
+import mshExtract
+import pdbReader
+from MainFrame import MFrame
+from constants import constants
+
+## open stl file
+print('exctracting geometry from file')
+path='D:/tre.stl'
+cons=gExtract.eSTL(path) # model=[[normal,vecn1,vecn2,vecn3],... ](triangles); cmass = (x,y,z)
+print('stl surface elements: ',len(cons.vect))
+##assign geometry
+cons.Gwidgets.append('geometry')
+print('model assigned to constants')
+
+
+## open mesh file
+print('exctracting geometry from file')
+path='D:/el_ex.txt'
+cons1=mshExtract.mExt(path)
+cons1.Gwidgets.append('mesh')
+print('stl surface elements: ',len(cons1.vect))
+
+## open mesh file
+print('exctracting geometry from file')
+path='D:/test.pdb'
+cons2=pdbReader.mExt(path)
+cons2.Gwidgets.append('mesh')
+print('stl surface elements: ',len(cons2.vect))
+
+c=[cons,cons1,cons2]
+## Initialize Main Frame with specific constants
+MFrame(c)
+
+#t = Thread(target=MFrame, args=(cons,))
+#t.start()
+#def r(cons):
+#    while 1:
+#        time.sleep(0.5)
+#        print('ok')
+#p = Thread(target=r, args=(cons,))
+#p.start()
+#p.join()
+#t.join()
+
+
+
